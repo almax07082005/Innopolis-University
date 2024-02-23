cd3d4ef1522b9c7d36076c21c328169f42fa59a3
Maxim Al Dandan <almaxim07082005@gmail.com>
2024-02-23 14:00:58 +0300

    Recreate C++17 TCS project to C++23

--- sem2-TCS/cpp-sem2-TCS/cmake-build-debug/cmake_install.cmake
+++ sem2-TCS/cpp-sem2-TCS/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/almax/Documents/Github/Innopolis-University/sem2-TCS/cpp-sem2-TCS
+# Install script for directory: C:/Users/almax/Documents/Github/Innopolis-University/sem2-TCS/Cpp-sem2-TCS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/cpp_sem2_TCS")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Cpp_sem2_TCS")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/almax/Documents/Github/Innopolis-University/sem2-TCS/cpp-sem2-TCS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/almax/Documents/Github/Innopolis-University/sem2-TCS/Cpp-sem2-TCS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
