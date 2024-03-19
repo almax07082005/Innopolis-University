c9ca686b220d196a425da2c353cc4a1109a7cf23
Maxim Al Dandan <almaxim07082005@gmail.com>
2024-03-19 19:52:06 +0300

    Fix task2 AGLA

--- sem2-AGLA/cpp-sem2-AGLA/task2.cpp
+++ sem2-AGLA/cpp-sem2-AGLA/task2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <memory>
 
 #define ERROR "Error: the dimensional problem occurred\n"
 typedef std::vector<std::vector<int>> vvint;
@@ -56,8 +57,8 @@ public:
         return cout;
     }
 
-    Matrix operator+(const Matrix& m) const {
-        Matrix res(rowsCount, columnsCount);
+    std::unique_ptr<Matrix> operator+(const Matrix& m) const {
+        auto res = std::make_unique<Matrix>(rowsCount, columnsCount);
 
         if (rowsCount != m.rowsCount || columnsCount != m.columnsCount) {
             throw std::invalid_argument(ERROR);
@@ -65,15 +66,15 @@ public:
 
         for (int i = 0; i < rowsCount; i++) {
             for (int j = 0; j < columnsCount; j++) {
-                res.matrix.at(i).at(j) = matrix.at(i).at(j) + m.matrix.at(i).at(j);
+                res->matrix.at(i).at(j) = matrix.at(i).at(j) + m.matrix.at(i).at(j);
             }
         }
 
         return res;
     }
 
-    Matrix operator-(const Matrix& m) const {
-        Matrix res(rowsCount, columnsCount);
+    std::unique_ptr<Matrix> operator-(const Matrix& m) const {
+        auto res = std::make_unique<Matrix>(rowsCount, columnsCount);
 
         if (rowsCount != m.rowsCount || columnsCount != m.columnsCount) {
             throw std::invalid_argument(ERROR);
@@ -81,15 +82,15 @@ public:
 
         for (int i = 0; i < rowsCount; i++) {
             for (int j = 0; j < columnsCount; j++) {
-                res.matrix.at(i).at(j) = matrix.at(i).at(j) - m.matrix.at(i).at(j);
+                res->matrix.at(i).at(j) = matrix.at(i).at(j) - m.matrix.at(i).at(j);
             }
         }
 
         return res;
     }
 
-    Matrix operator*(const Matrix& m) const {
-        Matrix res(rowsCount, m.columnsCount);
+    std::unique_ptr<Matrix> operator*(const Matrix& m) const {
+        auto res = std::make_unique<Matrix>(rowsCount, m.columnsCount);
 
         if (columnsCount != m.rowsCount) {
             throw std::invalid_argument(ERROR);
@@ -98,7 +99,7 @@ public:
         for (int i = 0; i < rowsCount; i++) {
             for (int j = 0; j < m.columnsCount; j++) {
                 for (int k = 0; k < columnsCount; k++) {
-                    res.matrix.at(i).at(j) += matrix.at(i).at(k) * m.matrix.at(k).at(j);
+                    res->matrix.at(i).at(j) += matrix.at(i).at(k) * m.matrix.at(k).at(j);
                 }
             }
         }
@@ -106,12 +107,12 @@ public:
         return res;
     }
 
-    Matrix operator~() const {
-        Matrix res(columnsCount, rowsCount);
+    std::unique_ptr<Matrix> operator~() const {
+        auto res = std::make_unique<Matrix>(columnsCount, rowsCount);
 
         for (int i = 0; i < rowsCount; i++) {
             for (int j = 0; j < columnsCount; j++) {
-                res.matrix.at(j).at(i) = matrix.at(i).at(j);
+                res->matrix.at(j).at(i) = matrix.at(i).at(j);
             }
         }
 
@@ -122,6 +123,7 @@ public:
 class SquareMatrix: public Matrix {
 public:
     explicit SquareMatrix(int size = 0): Matrix(size, size) {}
+
     friend std::istream& operator>>(std::istream& cin, SquareMatrix& m) {
         cin >> m.rowsCount;
         m.columnsCount = m.rowsCount;
@@ -137,6 +139,7 @@ public:
 
         return cin;
     }
+
     friend std::ostream& operator<<(std::ostream& cout, const SquareMatrix& m) {
         for (int i = 0; i < m.rowsCount; i++) {
             for (int j = 0; j < m.rowsCount; j++) {
@@ -146,6 +149,30 @@ public:
 
         return cout;
     }
+
+    std::unique_ptr<SquareMatrix> operator+(const SquareMatrix& m) const {
+        std::unique_ptr<Matrix> res = std::move(Matrix::operator+(m));
+        std::unique_ptr<SquareMatrix> result = std::make_unique<SquareMatrix>(*reinterpret_cast<SquareMatrix*>(res.get()));
+        return result;
+    }
+
+    std::unique_ptr<SquareMatrix> operator-(const SquareMatrix& m) const {
+        std::unique_ptr<Matrix> res = std::move(Matrix::operator-(m));
+        std::unique_ptr<SquareMatrix> result = std::make_unique<SquareMatrix>(*reinterpret_cast<SquareMatrix*>(res.get()));
+        return result;
+    }
+
+    std::unique_ptr<SquareMatrix> operator*(const SquareMatrix& m) const {
+        std::unique_ptr<Matrix> res = std::move(Matrix::operator*(m));
+        std::unique_ptr<SquareMatrix> result = std::make_unique<SquareMatrix>(*reinterpret_cast<SquareMatrix*>(res.get()));
+        return result;
+    }
+
+    std::unique_ptr<SquareMatrix> operator~() const {
+        std::unique_ptr<Matrix> res = std::move(Matrix::operator~());
+        std::unique_ptr<SquareMatrix> result = std::make_unique<SquareMatrix>(*reinterpret_cast<SquareMatrix*>(res.get()));
+        return result;
+    }
 };
 
 int main() {
@@ -153,24 +180,24 @@ int main() {
     std::cin >> A >> B >> C;
 
     try {
-        std::cout << A + B;
+        std::cout << *(A + B);
     } catch (const std::invalid_argument& exception) {
         std::cout << exception.what();
     }
 
     try {
-        std::cout << B - A;
+        std::cout << *(B - A);
     } catch (const std::invalid_argument& exception) {
         std::cout << exception.what();
     }
 
     try {
-        std::cout << C * A;
+        std::cout << *(C * A);
     } catch (const std::invalid_argument& exception) {
         std::cout << exception.what();
     }
 
-    std::cout << ~A;
+    std::cout << *~A;
 
     return EXIT_SUCCESS;
 }
