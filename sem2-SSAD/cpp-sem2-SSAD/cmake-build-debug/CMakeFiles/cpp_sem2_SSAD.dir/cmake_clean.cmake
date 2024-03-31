2d8180712d6cb18b5fdf3147f8d92ceec1107401
Maxim Al Dandan <almaxim07082005@gmail.com>
2024-04-01 00:44:34 +0300

    SSAD project rebuild

---
+++ sem2-SSAD/cpp-sem2-SSAD/cmake-build-debug/CMakeFiles/cpp_sem2_SSAD.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_sem2_SSAD.dir/main.cpp.obj"
+  "CMakeFiles/cpp_sem2_SSAD.dir/main.cpp.obj.d"
+  "cpp_sem2_SSAD.exe"
+  "cpp_sem2_SSAD.exe.manifest"
+  "cpp_sem2_SSAD.pdb"
+  "libcpp_sem2_SSAD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_sem2_SSAD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sem2-SSAD/cpp-sem2-SSAD/cmake-build-debug/CMakeFiles/cpp_sem2_SSAD.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cpp_sem2_SSAD.
+# This may be replaced when dependencies are built.
